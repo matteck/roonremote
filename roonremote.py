1bc12a3858a0cdaee86e1d4f693315f5e6ee76b0
root <Matt Eckhaus>
2018-05-11 10:04:03 +0100

    Added local harmonyhub-cli commands

--- roonremote.py
+++ roonremote.py
@@ -2,10 +2,14 @@
 import evdev
 import select
 import requests
+import subprocess
 
-base_url = "http://192.168.1.11:3000/api/v1"
+
+base_url = "http://roonstation:3000/api/v1"
+myzone = "Hifi"
 devices = {}
 for fn in evdev.list_devices():
+    print(fn)
     dev = evdev.InputDevice(fn)
     if dev.name.find('HBGIC') >= 0:
         devices[dev.fd] = dev
@@ -15,6 +19,7 @@ while True:
     for fd in r:
         for event in devices[fd].read():
             url = None
+            cmd = None
             if event.type == evdev.ecodes.EV_KEY:
                 keyev = evdev.categorize(event)
                 code = keyev.keycode[4:]
@@ -28,25 +33,27 @@ while True:
                 print(code, state)
                 if state == "DOWN":
                     if code == "PLAYPAUSE":
-                        url = "%s/zone/current/control/playpause" % base_url
+                        url = "%s/zone/%s/control/playpause" % (base_url, myzone)
                     elif code == "STOP":
                         url = "%s/zone/all/control/pause" % base_url
+                        #url = "%s/zone/%s/control/stop" % (base_url, myzone)
                     elif code == "REWIND":
-                        url = "%s/zone/current/control/previous" % base_url
+                        url = "%s/zone/%s/control/previous" % (base_url, myzone)
                     elif code == "FASTFORWARD":
-                        url = "%s/zone/current/control/next" % base_url
-                    elif code == "UP":
-                        url = "%s/zone/current/volume/relative_step/1" % base_url
-                    elif code == "DOWN":
-                        url = "%s/zone/current/volume/relative_step/-1" % base_url
-                if state == "HOLD":
+                        url = "%s/zone/%s/control/next" % (base_url, myzone)
+                    elif code == "INFO":
+                        cmd = ['/root/harmonyHubCLI/bin/harmonyhub-cli', '-l', 'HarmonyHub', '-d', 'Channel Islands Audio Amp', '-c', 'Mute']
+                if state == "HOLD" or state == "DOWN":
                     if code == "UP":
-                        url = "%s/zone/current/volume/relative_step/1" % base_url
+                        cmd = ['/root/harmonyHubCLI/bin/harmonyhub-cli', '-l', 'HarmonyHub', '-a', 'Roon', '-c', 'Volume,Volume Up']
                     elif code == "DOWN":
-                        url = "%s/zone/current/volume/relative_step/-1" % base_url
+                        cmd = ['/root/harmonyHubCLI/bin/harmonyhub-cli', '-l', 'HarmonyHub', '-a', 'Roon', '-c', 'Volume,Volume Down']
             if url:
                 print(url)
                 try:
-                    req = requests.put(url)
+                    req = requests.get(url)
                 except:
                     print("Request to %s failed" % (url))
+            if cmd:
+                print(" ".join(cmd))
+                subprocess.call(cmd)
