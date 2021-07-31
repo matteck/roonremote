8e2dbc24d06d305fb703100db62a520c57766e9a
Matthew Eckhaus <matt@eckha.us>
2021-07-31 16:19:11 +1000

    Timer

--- roonautoshutdown.py
+++ roonautoshutdown.py
@@ -4,12 +4,14 @@ import roonapi
 import subprocess
 from pathlib import Path
 import time
+import logging
 
 server = "192.168.50.125"
 zone_name = "iFi"
 token_file = "/root/.roon_token_save"
 flag_file = "/root/.roon_playing_flag"
-shutdown_delay_min = 1
+shutdown_delay_min = 10
+this_zone_name = "iFi"
 
 appinfo = {
     "extension_id": "roonshutdown",
@@ -25,12 +27,12 @@ try:
 except FileNotFoundError:
     token = None
 
+roonapi.LOGGER.setLevel(logging.WARN)
 api = roonapi.RoonApi(appinfo, token, server)
 
 with open(token_file, "w") as f:
     f.write(api.token)
 
-this_zone_name = "iFi"
 
 zones = api.zones
 playing = False
@@ -43,12 +45,17 @@ for output in zones.values():
 if playing == True:
     with open(flag_file, "w") as f:
         f.write(str(int(time.time())))
+        print("Playing. Writing ", str(int(time.time())))
 else:
     try:
         with open(flag_file, "r") as f:
             timestamp = int(f.read())
         if timestamp < time.time() - shutdown_delay_min * 60:
             subprocess.run(['shutdown','-P','now'])
+            print("Shutting down PortPi")
+        else:
+            print("Nimer not expired")
     except FileNotFoundError:
         with open(flag_file, "w") as f:
             f.write(str(int(time.time())))
+            print("No file. Writing ", str(int(time.time())))
