3ab51b998eca36305c2a81c774e7b407bdaaa15c
Matthew Eckhaus <matt@eckha.us>
2021-07-31 17:30:39 +1000

    Use ctime for timer

--- roonautoshutdown.py
+++ roonautoshutdown.py
@@ -10,7 +10,7 @@ server = "192.168.50.125"
 zone_name = "iFi"
 token_file = "/root/.roon_token_save"
 flag_file = "/root/.roon_playing_flag"
-shutdown_delay_min = 10
+shutdown_delay_min = 20
 this_zone_name = "iFi"
 
 appinfo = {
@@ -20,6 +20,8 @@ appinfo = {
     "publisher": "Matthew Eckhaus",
     "email": "matteck@github",
 }
+
+flag_path = Path(flag_file)
 
 # Can be None if you don't yet have a token
 try:
@@ -44,18 +46,16 @@ for output in zones.values():
 
 if playing == True:
     with open(flag_file, "w") as f:
-        f.write(str(int(time.time())))
-        print("Playing. Writing ", str(int(time.time())))
+        flag_path.touch()
 else:
     try:
         with open(flag_file, "r") as f:
-            timestamp = int(f.read())
-        if timestamp < time.time() - shutdown_delay_min * 60:
+            # get ctime
+            ctime = flag_path.stat().st_ctime
+        if ctime < time.time() - shutdown_delay_min * 60:
+            flag_path.unlink()
             subprocess.run(['shutdown','-P','now'])
             print("Shutting down PortPi")
-        else:
-            print("Nimer not expired")
     except FileNotFoundError:
-        with open(flag_file, "w") as f:
-            f.write(str(int(time.time())))
-            print("No file. Writing ", str(int(time.time())))
+        print("Flag file not found. Creating", flag_file)
+        flag_path.touch()
